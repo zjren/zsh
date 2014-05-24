3793a8c3dfe6d0252a573b4939b3ef9eca36162c
kisspy <kisspying@gmail.com>
2014-05-24 22:51:10 +0800

    init zsh install project

---
+++ install_for_ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#安装zsh
+sudo apt-get install zsh
+
+cd ~
+
+#设置当前用户使用zsh
+chsh -s /bin/zsh
+
+#安装git
+sudo brew install git
+
+#安装oh myzsh
+
+#自动安装
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+#手动安装
+# git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+#配置zsh
+
+wget https://github.com/zjren/zsh/raw/master/.zshrc -o ~/.zshrc
+
+echo "zsh install success"
